13ee7b61693d9263194075dde3d32f079912472c
Auleiy <Auleiy@outlook.com>
2025-03-11 19:47:45 +0800

    重命名不完全

--- src/main/java/io/naustudio/lostland/item/ItemBibleOldTesta.java
+++ src/main/java/io/naustudio/lostland/item/ItemBibleOldTesta.java
@@ -18,7 +18,7 @@ public class ItemBibleOldTesta extends ShieldItem {
     public void appendHoverText(@NotNull ItemStack item, @NotNull TooltipContext context,
                                 @NotNull List<Component> content, @NotNull TooltipFlag flags) {
         super.appendHoverText(item, context, content, flags);
-        content.add(Component.translatable("item.lostfaith.bible_old_testa.description.line1"));
-        content.add(Component.translatable("item.lostfaith.bible_old_testa.description.line2"));
+        content.add(Component.translatable("item.lostland.bible_old_testa.description.line1"));
+        content.add(Component.translatable("item.lostland.bible_old_testa.description.line2"));
     }
 }
